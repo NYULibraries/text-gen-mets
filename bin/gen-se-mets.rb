0203f88953c1429c205a8f0cfe97995fa5f7e9a1
Joseph Pawletko <jgpawletko@gmail.com>
2014-03-19 12:20:17 -0400

    emit_mets_open_tag working

--- bin/gen-se-mets.rb
+++ bin/gen-se-mets.rb
@@ -23,18 +23,19 @@
 
 
 def emit_xml_header
-  puts <<'HERE_DOGGY' 
+  puts <<'HERE_DOC_EOF' 
 <?xml version="1.0" encoding="UTF-8"?> 
-HERE_DOGGY
+HERE_DOC_EOF
 end
 
-# def emit_mets_open_tag(objid)
-#   puts<<HERE_DOC_EOF
-#     \<mets xmlns="http://www.loc.gov/METS/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-#     xsi:schemaLocation="http://www.loc.gov/METS/ http://www.loc.gov/standards/mets/version191/mets.xsd" xmlns:xlink="http://www.w3.org/1999/xlink"
-#   xmlns:mods="http://www.loc.gov/mods/v3" OBJID="#{objid}"\>
-# HERE_DOC_EOF
-# end
+def emit_mets_open_tag(obj_id)
+  puts <<-'HERE_DOC_EOF'
+    <mets xmlns="http://www.loc.gov/METS/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.loc.gov/METS/ http://www.loc.gov/standards/mets/version191/mets.xsd" xmlns:xlink="http://www.w3.org/1999/xlink"
+  HERE_DOC_EOF
+
+  puts "    xmlns:mods=\"http://www.loc.gov/mods/v3\" OBJID=\"#{obj_id}\">"
+end
 
 # def emit_mets_hdr(create  = Time.now.utc.strftime("%FT%TZ"), 
 #                   lastmod = Time.now.utc.strftime("%FT%TZ"),
@@ -62,17 +63,18 @@ end
   
 
 #-------------------------------------------------------------------------------
-src_dir = ARGV[0]
-tgt_dir = ARGV[1] || src_dir
+obj_id  = ARGV[0]
+src_dir = ARGV[1]
+tgt_dir = ARGV[2] || src_dir
 
 puts "src_dir = #{src_dir}"
 puts "tgt_dir = #{tgt_dir}"
 
 assert_file_inventory(src_dir)
 emit_xml_header
+emit_mets_open_tag(obj_id)
 
 exit
-emit_mets_open_tag
 emit_mets_hdr
 
 exit 0
