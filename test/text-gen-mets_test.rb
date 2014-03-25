2dd4688cbfc600afe4206c10dde37cb390c6ff29
Joseph Pawletko <jgpawletko@gmail.com>
2014-03-25 12:54:38 -0400

    added se type test

--- test/text-gen-mets_test.rb
+++ test/text-gen-mets_test.rb
@@ -3,13 +3,18 @@ require 'open3'
 
 class TestTextGenMets < Test::Unit::TestCase
 
+  def test_exit_status_with_valid_invocation
+    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'VERTICAL' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' test/text")
+    assert(s == 0, "incorrect exit status")
+    assert_match(/<mets xmlns/, o, "no mets output detected")
+  end
+
   def test_exit_status_with_incorrect_argument_count
     o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb")
     assert(s != 0, "incorrect argument count")
     assert(o == '')
     assert_match(/incorrect number of arguments/, e, 'unexpected error message')
   end
-
 
   def test_exit_status_with_invalid_dir
     o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'VERTICAL' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' invalid-dir-path")
@@ -18,10 +23,11 @@ class TestTextGenMets < Test::Unit::TestCase
     assert_match(/directory does not exist/, e, 'unexpected error message')
   end
 
-  def test_exit_status_with_valid_dir
-    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'VERTICAL' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' test/text")
-    assert(s == 0, "incorrect exit status")
-    assert_match(/<mets xmlns/, o, "no mets output detected")
+  def test_invalid_se_type
+    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'INVALID' 'VERTICAL' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' test/text")
+    assert(s != 0)
+    assert(o == '')
+    assert_match(/incorrect se type/, e, 'unexpected error message')
   end
 
 end
