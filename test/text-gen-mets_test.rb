cf4a8fbcd0d91293ed64d23a862c6e353d40b35d
Joseph Pawletko <jgpawletko@gmail.com>
2014-03-25 11:55:07 -0400

    initial test working

--- test/text-gen-mets_test.rb
+++ test/text-gen-mets_test.rb
@@ -1,8 +1,9 @@
 require 'test/unit'
 class TestHello < Test::Unit::TestCase
 
-  def test_hello
-    puts "HELLO!"
+  def test_exit_status
+    `ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'VERTICAL' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' test/text > /dev/null`
+    assert($? == 0, "incorrect exit status")
   end
 
 end
