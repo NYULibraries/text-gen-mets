2a805b7d38d0ed7a64e09a81722717367f05ff09
jgpawletko <jgpawletko@gmail.com>
2016-03-11 16:21:35 -0500

    add tests for Filenames without extensions

--- test/filename_test.rb
+++ test/filename_test.rb
@@ -2,10 +2,11 @@ require 'test_helper'
 
 class FilenameTest < MiniTest::Unit::TestCase
 
-  attr_accessor :filename
+  attr_accessor :filename, :filename_no_ext
   
   def setup
-    @filename = Filename.new('a/b/c/foo_d.tif')
+    @filename        = Filename.new('a/b/c/foo_d.tif')
+    @filename_no_ext = Filename.new('d/e/f/bar_d')
   end
 
   def test_path
@@ -18,9 +19,19 @@ class FilenameTest < MiniTest::Unit::TestCase
     assert_equal expected, filename.extension
   end
     
+  def test_extension_no_extension
+    expected = ''
+    assert_equal expected, filename_no_ext.extension
+  end
+
   def test_rootname
     expected = 'foo_d'
     assert_equal expected, filename.rootname
   end
-  
+
+  def test_rootname_no_extension
+    expected = 'bar_d'
+    assert_equal expected, filename_no_ext.rootname
+  end
+
 end
