3ae23b3ffeee048134dc5423e81eaf2d5023bf04
jgpawletko <jgpawletko@gmail.com>
2016-03-12 15:38:32 -0500

    add BookSlot#label

--- lib/structure/book_slot.rb
+++ lib/structure/book_slot.rb
@@ -1,6 +1,15 @@
 # class handles book digitization slots
+# objects of this class respond to the following messages:
+#  label
+#  masters -> array of filename objects
+#  dmakers -> array of filename objects
+#  valid?
 
 module Structure
   class BookSlot
+    attr_accessor :label
+    def initialize(args = {})
+      @label = args[:label] || ''
+    end
   end
 end
--- test/structure/book_slot_test.rb
+++ test/structure/book_slot_test.rb
@@ -2,9 +2,21 @@ require 'test_helper'
 
 class Structure::BookSlotTest < MiniTest::Unit::TestCase
 
-  attr_accessor :slot
+  attr_accessor :slot, :slot_with_label
   
   def setup
+    @slot = Structure::BookSlot.new()
+    @slot_with_label = Structure::BookSlot.new(label: 'howdy!')
+
   end
 
+  def test_empty_label
+    expected = ''
+    assert_equal expected, slot.label
+  end
+
+  def test_non_empty_label
+    expected = 'howdy!'
+    assert_equal expected, slot_with_label.label
+  end
 end
