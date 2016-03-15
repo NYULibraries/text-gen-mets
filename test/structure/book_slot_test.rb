f6a2c447b6c79948914e09a031f95fa4d9ac116d
jgpawletko <jgpawletko@gmail.com>
2016-03-15 11:08:47 -0400

    refactor BookSlot tests

--- test/structure/book_slot_test.rb
+++ test/structure/book_slot_test.rb
@@ -1,17 +1,25 @@
 require 'test_helper'
-
+# test class for BookSlot
 class Structure::BookSlotTest < MiniTest::Unit::TestCase
 
-  attr_accessor :slot, :slot_with_label, :master, :dmaker, :master2, :dmaker2, :unknown
-  
+  attr_accessor :slot, :slot_with_label, :master, :dmaker, :master2, :dmaker2,
+                :unknown, :slot_multiple
+
   def setup
-    @slot = Structure::BookSlot.new()
+    @slot = Structure::BookSlot.new
     @slot_with_label = Structure::BookSlot.new(label: 'howdy!',
                                                name:  'nyu_aco000123_n0009897')
     @master  = Filename.new('a_m.tif')
     @dmaker  = Filename.new('a_d.tif')
     @master2 = Filename.new('b_m.tif')
     @dmaker2 = Filename.new('b_d.tif')
+
+    @slot_multiple = Structure::BookSlot.new
+    slot_multiple.add(dmaker)
+    slot_multiple.add(master)
+    slot_multiple.add(dmaker2)
+    slot_multiple.add(master2)
+
     @unknown = Filename.new('c_blerf.xyz')
   end
 
@@ -28,13 +36,12 @@ class Structure::BookSlotTest < MiniTest::Unit::TestCase
   def test_name
     expected = 'nyu_aco000123_n0009897'
     assert_equal expected, slot_with_label.name
-  end    
+  end
 
   def test_label
     expected = 'nyu_aco000123_n0009897'
     assert_equal expected, slot_with_label.name
   end
-
 
   def test_default_filenames
     expected = {}
@@ -42,24 +49,20 @@ class Structure::BookSlotTest < MiniTest::Unit::TestCase
   end
 
   def test_add_master
-    expected = {master: [master] }
+    expected = { master: [master] }
     slot.add(master)
     assert_equal expected, slot.filenames
   end
 
   def test_add_dmaker
-    expected = {dmaker: [dmaker] }
+    expected = { dmaker: [dmaker] }
     slot.add(dmaker)
     assert_equal expected, slot.filenames
   end
 
   def test_add_multiple
-    expected = {dmaker: [dmaker, dmaker2], master: [master, master2] }
-    slot.add(dmaker)
-    slot.add(master)
-    slot.add(dmaker2)
-    slot.add(master2)
-    assert_equal expected, slot.filenames
+    expected = { dmaker: [dmaker, dmaker2], master: [master, master2] }
+    assert_equal expected, slot_multiple.filenames
   end
 
   def test_exception_on_invalid_role
