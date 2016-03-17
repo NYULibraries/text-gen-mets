ddb6a399946958166eafe4d63ff11f106d0df7e2
jgpawletko <jgpawletko@gmail.com>
2016-03-17 11:18:24 -0400

    add SlotList exception when dmakers nil

--- lib/structure/slot_list.rb
+++ lib/structure/slot_list.rb
@@ -1,5 +1,14 @@
 module Structure
   # class deals with aggregations of Slots
+  # initialize with masters and dmakers
+  # 
   class SlotList
+    attr_reader :dmakers, :masters
+    def initialize(args)
+      @dmakers = args.dmakers
+      @masters = args.masters
+
+      raise(ArgumentError, 'dmakers cannot be nil') unless dmakers
+    end
   end
 end
--- test/structure/slot_list_test.rb
+++ test/structure/slot_list_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
+require 'ostruct'
 # test class for SlotList
 class Structure::SlotListTest < MiniTest::Unit::TestCase
 
+  attr_accessor :slot_list, :args
   def setup
-    @slot_list = Structure::SlotList.new
+    @args = OpenStruct.new
+  end
+
+  def test_incoming_arguments_dmakers_nil
+    args.masters = []
+    assert_raises(ArgumentError) { Structure::SlotList.new(args) }
   end
 end
