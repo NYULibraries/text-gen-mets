f7a52583382d72b5baf93decd6af2c62d62cfda8
jgpawletko <jgpawletko@gmail.com>
2016-03-17 10:43:28 -0400

    add Structure::SlotList skeleton

---
+++ lib/structure/slot_list.rb
@@ -0,0 +1,5 @@
+module Structure
+  # class deals with aggregations of Slots
+  class SlotList
+  end
+end
---
+++ test/structure/slot_list_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+# test class for SlotList
+class Structure::SlotListTest < MiniTest::Unit::TestCase
+
+  def setup
+    @slot_list = Structure::SlotList.new
+  end
+end
