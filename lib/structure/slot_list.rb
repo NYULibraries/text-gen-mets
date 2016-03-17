4a2c5da72973e71f98306cfae39883a996771196
jgpawletko <jgpawletko@gmail.com>
2016-03-17 12:12:12 -0400

    snapshot while refactoring

--- lib/structure/slot_list.rb
+++ lib/structure/slot_list.rb
@@ -3,13 +3,25 @@ module Structure
   # initialize with masters and dmakers
   # 
   class SlotList
-    attr_reader :dmakers, :masters
+    attr_reader :dmakers, :masters, :slot_class
     def initialize(args)
       @dmakers = args.dmakers
       @masters = args.masters
+      @slot_class = args.slot_class
 
       raise(ArgumentError, 'dmakers cannot be nil') unless dmakers
       raise(ArgumentError, 'masters cannot be nil') unless masters
+      raise(ArgumentError, 'slot_class cannot be nil') unless slot_class
+    end
+
+    def slots
+      @slots ||= gen_slots
+    end
+
+    private
+
+    def gen_slots
+#      result = dmakers.collect { |dmaker| slot_class.new(dmaker) }
     end
   end
 end
--- test/structure/slot_list_test.rb
+++ test/structure/slot_list_test.rb
@@ -3,9 +3,36 @@ require 'ostruct'
 # test class for SlotList
 class Structure::SlotListTest < MiniTest::Unit::TestCase
 
-  attr_accessor :slot_list, :args
+  attr_accessor :slot_list, :args, :dmakers, :masters, :slots,
+                :dmaker1, :dmaker2, :master1, :master2
   def setup
     @args = OpenStruct.new
+    @args.slot_class = Structure::BookSlot
+
+    @dmaker1 = Filename.new('j/k/x_d.tif')
+    @dmaker2 = Filename.new('j/k/y_d.tif')
+    @dmakers = [dmaker1, dmaker2]
+
+    @master1 = Filename.new('j/k/x_m.tif')
+    @master2 = Filename.new('j/k/y_m.tif')
+    @masters = [master1, master2]
+
+    @slots   = begin
+                 one = Structure::BookSlot.new
+                 one.add(dmaker1)
+                 one.add(master1)
+                 two = Structure::BookSlot.new
+                 two.add(dmaker2)
+                 two.add(master2)
+                 [one, two]
+               end
+  end
+
+  def test_incoming_arguments_slot_class_nil
+    args.masters = []
+    args.dmakers = []
+    args.slot_class = nil
+    assert_raises(ArgumentError) { Structure::SlotList.new(args) }
   end
 
   def test_incoming_arguments_dmakers_nil
@@ -16,5 +43,18 @@ class Structure::SlotListTest < MiniTest::Unit::TestCase
   def test_incoming_arguments_masters_nil
     args.dmakers = []
     assert_raises(ArgumentError) { Structure::SlotList.new(args) }
+  end
+
+  def test_incoming_arguments_masters_nil
+    args.dmakers = []
+    assert_raises(ArgumentError) { Structure::SlotList.new(args) }
+  end
+
+  def test_slots
+    skip('pending refactor of BookSlot')
+    args.dmakers = dmakers
+    args.masters = masters
+    sl = Structure::SlotList.new(args)
+    assert_equal(slots, sl.slots)
   end
 end
