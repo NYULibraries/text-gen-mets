4d7c4152859b42746522141389a665ce694be462
jgpawletko <jgpawletko@gmail.com>
2016-03-21 12:58:02 -0400

    add Structure#SlotList

--- lib/structure/slot_list.rb
+++ lib/structure/slot_list.rb
@@ -1,29 +1,73 @@
 module Structure
   # class deals with aggregations of Slots
   # initialize with masters and dmakers
-  # 
+  #
+  # messages:
+  # SlotList.new passed dmakers and masters
+  # create slots
+  # load dmakers into slots
+  # load masters into slots
+  # if slot not found for master
   class SlotList
     attr_reader :dmakers, :masters, :slot_class
+    attr_accessor :slots
     def initialize(args)
       @dmakers = args.dmakers
       @masters = args.masters
       @slot_class = args.slot_class
+      @slots = {}
 
       raise(ArgumentError, 'dmakers cannot be nil') unless dmakers
       raise(ArgumentError, 'masters cannot be nil') unless masters
       raise(ArgumentError, 'slot_class cannot be nil') unless slot_class
+
+      populate_slots
     end
 
-    def slots
-      @slots ||= gen_slots
+    def ==(other)
+      result &&= (self.dmakers == other.dmakers)
+      result
     end
 
     private
 
-    def gen_slots
-      result = dmakers.collect do |dmaker|
-        slot_class.new(name: dmaker.rootname_minus_role)
+    def populate_slots
+      create_slots
+      load_dmakers
+      load_masters
+    end
+
+    def create_slots
+      dmakers.each do |dmaker|
+        slot_name = dmaker_to_slot_name(dmaker)
+        slots[slot_name] = slot_class.new(name: slot_name)
       end
+    end
+
+    def load_dmakers
+      dmakers.each do |dmaker|
+        slot_name = dmaker_to_slot_name(dmaker)
+        slot = slots[slot_name]
+        raise(RuntimeError, "missing slot for #{slot_name}") unless slot
+        slot.add(dmaker)
+      end
+    end
+
+    def load_masters
+      masters.each do |master|
+        slot_name = master_to_slot_name(master)
+        slot = slots[slot_name]
+        raise(RuntimeError, "missing slot for #{slot_name}") unless slot
+        slot.add(master)
+      end
+    end
+
+    def dmaker_to_slot_name(dmaker)
+      dmaker.rootname_minus_role
+    end
+
+    def master_to_slot_name(master)
+      master.rootname_minus_role
     end
   end
 end
--- test/structure/slot_list_test.rb
+++ test/structure/slot_list_test.rb
@@ -1,55 +1,67 @@
 require 'test_helper'
 require 'ostruct'
-# test class for SlotList
-class Structure::SlotListTest < MiniTest::Unit::TestCase
+module Structure
+  # test class for SlotList
+  class SlotListTest < MiniTest::Test
+    attr_accessor :args, :dmakers, :masters, :slots, :slots1
 
-  attr_accessor :slot_list, :args, :dmakers, :masters, :slots
+    def setup
+      @args = OpenStruct.new
+      @args.slot_class = Structure::BookSlot
 
-  def setup
-    @args = OpenStruct.new
-    @args.slot_class = Structure::BookSlot
+      @dmakers = ['j/k/x_d.tif', 'j/k/y_d.tif'].collect { |d| Filename.new(d) }
+      @masters = ['j/k/x_m.tif', 'j/k/y_m.tif'].collect { |m| Filename.new(m) }
 
-    @dmakers = ['j/k/x_d.tif', 'j/k/y_d.tif'].collect { |d| Filename.new(d) }
-    @masters = ['j/k/x_m.tif', 'j/k/y_m.tif'].collect { |m| Filename.new(m) }
+      @slots = begin
+                     a = {}
+                     dmakers.each_index do |i|
+                       slot_name = dmakers[i].rootname_minus_role
+                       a[slot_name] = Structure::BookSlot.new(name: slot_name)
+                       a[slot_name].add(dmakers[i])
+                       a[slot_name].add(masters[i])
+                     end
+                     a
+                   end
 
-    @slots   = begin
-                 a = []
-                 dmakers.each_index do |i|
-                   a[i] = Structure::BookSlot.new
-                   a[i].add(dmakers[i])
-                   a[i].add(masters[i])
-                 end
-                 a
-               end
-  end
+      @slots1 = begin
+                      a = {}
+                      dmakers.each_index do |i|
+                        slot_name = dmakers[i].rootname_minus_role
+                        a[slot_name] = Structure::BookSlot.new(name: slot_name)
+                        a[slot_name].add(dmakers[i])
+                        a[slot_name].add(masters[i])
+                      end
+                      a
+                    end
+    end
 
-  def test_incoming_arguments_slot_class_nil
-    args.masters = []
-    args.dmakers = []
-    args.slot_class = nil
-    assert_raises(ArgumentError) { Structure::SlotList.new(args) }
-  end
+    def test_incoming_arguments_slot_class_nil
+      args.masters = []
+      args.dmakers = []
+      args.slot_class = nil
+      assert_raises(ArgumentError) { Structure::SlotList.new(args) }
+    end
 
-  def test_incoming_arguments_dmakers_nil
-    args.masters = []
-    assert_raises(ArgumentError) { Structure::SlotList.new(args) }
-  end
+    def test_incoming_arguments_dmakers_nil
+      args.masters = []
+      assert_raises(ArgumentError) { Structure::SlotList.new(args) }
+    end
 
-  def test_incoming_arguments_masters_nil
-    args.dmakers = []
-    assert_raises(ArgumentError) { Structure::SlotList.new(args) }
-  end
+    def test_incoming_arguments_masters_nil
+      args.dmakers = []
+      assert_raises(ArgumentError) { Structure::SlotList.new(args) }
+    end
 
-  def test_incoming_arguments_masters_nil
-    args.dmakers = []
-    assert_raises(ArgumentError) { Structure::SlotList.new(args) }
-  end
+    def test_incoming_arguments_masters_nil
+      args.dmakers = []
+      assert_raises(ArgumentError) { Structure::SlotList.new(args) }
+    end
 
-  def test_slots
-    skip('pending refactor of BookSlot')
-    args.dmakers = dmakers
-    args.masters = masters
-    sl = Structure::SlotList.new(args)
-    assert_equal(slots, sl.slots)
+    def test_slots
+      args.dmakers = dmakers
+      args.masters = masters
+      sl = Structure::SlotList.new(args)
+      assert_equal(slots, sl.slots)
+    end
   end
 end
